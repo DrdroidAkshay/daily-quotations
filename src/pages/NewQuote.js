58b0ad17c3f3f175a17ba3655309c6805bade3fa
Akshay <akshayyadav04031998@gmail.com>
2021-06-27 16:07:56 +0530

    Implementing programmatic navigation using useHistory

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,7 +1,11 @@
+import { useHistory } from "react-router-dom";
 import QuoteForm from "../components/quotes/QuoteForm";
 const NewQuote = () => {
+  const history = useHistory();
   const addQuoteHandler = (quotedetails) => {
     console.log(quotedetails.author, quotedetails.text);
+
+    history.push("./quotes");
   };
   return <QuoteForm onAddQuote={addQuoteHandler} />;
 };
