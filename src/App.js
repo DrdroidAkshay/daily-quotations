084bb805587195d4b044ba7efa589f10b117f895
Akshay <akshayyadav04031998@gmail.com>
2021-06-27 15:54:31 +0530

    Adding a not found page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import QuoteDetail from "./pages/QuoteDetail";
 import NewQuote from "./pages/NewQuote";
 import React from "react";
 import Layout from "./components/layout/Layout";
+import NotFound from "./pages/NotFound";
 function App() {
   return (
     <Layout>
@@ -19,6 +20,9 @@ function App() {
         </Route>
         <Route path="/new-quote" exact>
           <NewQuote />
+        </Route>
+        <Route path="*">
+          <NotFound />
         </Route>
       </Switch>
     </Layout>
