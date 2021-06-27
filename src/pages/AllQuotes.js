2661e4d24c9ea121bde5dac2deae6d0bcfcbf3e9
Akshay <akshayyadav04031998@gmail.com>
2021-06-27 15:46:47 +0530

    Outputting data on the details page

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,8 +1,8 @@
 import QuoteList from "../components/quotes/QuoteList";
 const DUMMY_QUOTES = [
-  { id: 1, author: "Akshay", text: "Learning React is fun." },
-  { id: 2, author: "Shiva", text: "Learning HTML is fun." },
-  { id: 3, author: "Kriss", text: "Learning JAVA is fun." },
+  { id: "1", author: "Akshay", text: "Learning React is fun." },
+  { id: "2", author: "Shiva", text: "Learning HTML is fun." },
+  { id: "3", author: "Kriss", text: "Learning JAVA is fun." },
 ];
 const AllQuotes = () => {
   return <QuoteList quotes={DUMMY_QUOTES} />;
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,13 +1,25 @@
 import React from "react";
 import { Route, useParams } from "react-router-dom";
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+  { id: "1", author: "Akshay", text: "Learning React is fun." },
+  { id: "2", author: "Shiva", text: "Learning HTML is fun." },
+  { id: "3", author: "Kriss", text: "Learning JAVA is fun." },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
+  console.log(params.quoteId);
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+  console.log(quote);
+  if (!quote) {
+    return <p>No quote found</p>;
+  }
   return (
     <React.Fragment>
-      <h1>Quote Detail</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
