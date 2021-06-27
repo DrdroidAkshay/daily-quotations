103d014982fd4beb2a920a1e5d7d2b86ed35e0ef
Akshay <akshayyadav04031998@gmail.com>
2021-06-27 18:16:46 +0530

    Adding quotes to database

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,12 +1,21 @@
+import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import QuoteForm from "../components/quotes/QuoteForm";
+import useHttp from "../components/hooks/use-http";
+import { addQuote } from "../components/lib/api";
 const NewQuote = () => {
+  const { sendRequest, status } = useHttp(addQuote);
   const history = useHistory();
-  const addQuoteHandler = (quotedetails) => {
-    console.log(quotedetails.author, quotedetails.text);
-
-    history.push("./quotes");
+  useEffect(() => {
+    if (status === "completed") {
+      history.push("./quotes");
+    }
+  }, [status, history]);
+  const addQuoteHandler = (quoteData) => {
+    sendRequest(quoteData);
   };
-  return <QuoteForm onAddQuote={addQuoteHandler} />;
+  return (
+    <QuoteForm isLoading={status === "pending"} onAddQuote={addQuoteHandler} />
+  );
 };
 export default NewQuote;
